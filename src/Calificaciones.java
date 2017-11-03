96c758aeaeaa12c3000ec764b91895f85e664ed0
claudialm <claudia_incondicional@hotmail.com>
2017-11-02 21:19:41 -0300

    Operador de promedio

--- src/Calificaciones.java
+++ src/Calificaciones.java
@@ -12,15 +12,18 @@ public class Calificaciones
 {
   public static void main(String[] args) 
   {
-  float nota1, nota2, nota3, promedio;
+  float nota1; 
+  float nota2; 
+  float nota3; 
+  float promedio;
   
    
-  nota1 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese un nota"));
-  nota2 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese un nota"));
-  nota3 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese un nota"));
-  promedio = Float.parseFloat(JOptionPane.showInputDialog("Inegrese promedio"));
+  nota1 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese un nota1"));
+  nota2 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese un nota2"));
+  nota3 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese un nota3"));
+  promedio = (nota1+nota2+nota3)/3;
     
-      if (promedio  > 4) 
+      if (promedio > 4) 
       {
           JOptionPane.showMessageDialog(null, "Aprobo");
       }
