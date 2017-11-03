ab6bc202201d62bc0391c94cc9a833ed05f1c9fa
claudialm <claudia_incondicional@hotmail.com>
2017-11-02 21:01:33 -0300

    Esta clase recibe notas por ventana

--- src/Calificaciones.java
+++ src/Calificaciones.java
@@ -12,14 +12,22 @@ public class Calificaciones
 {
   public static void main(String[] args) 
   {
-  float nota1, nota2, nota3;
-  float promedio=(nota1+nota2+nota3/3);
+  float nota1, nota2, nota3, promedio;
+  
    
   nota1 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese un nota"));
   nota2 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese un nota"));
   nota3 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese un nota"));
+  promedio = Float.parseFloat(JOptionPane.showInputDialog("Inegrese promedio"));
     
-       
+      if (promedio  > 4) 
+      {
+          JOptionPane.showMessageDialog(null, "Aprobo");
+      }
+      else 
+      {
+      JOptionPane.showMessageDialog(null, "Reprobo");
+      }
       
           
       
